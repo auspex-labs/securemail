2ec58bb4756390927eacc84e8f6571bb54ba17c2
Wyatt <wyatt@practicalcode.net>
2019-01-02 14:13:06 -0500

    Finished dmarc installation first pass; DNS recommendations done; Testing and runthrough needed

--- script_1.py
+++ script_1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+#https://petermolnar.net/howto-spf-dkim-dmarc-postfix/
+#https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-postfix-on-ubuntu-18-04
+
 import apt
 import os
 import lsb_release as lsb
@@ -106,17 +109,32 @@ def install_mail_client(mailbox):
 
 def install_opendmarc():
     os.system("apt install opendmarc")
+    try:
+        os.mkdir("/etc/opendmarc")
+    except:
+        pass
 
 def install_opendkim():
     os.system("apt install opendkim opendkim-tools")
+    try:
+        os.mkdir("/etc/opendkim")
+    except:
+        pass
 
-def dns_recommendation_spf():
+def get_domain_name():
     domain_name = input("Input your domain name (e.g. domain.com): ")
-    print("Add the following to your DNS record:")
-    print('*.' + domain_name + '. 1800 IN TXT "v=spf1 mx ip4:YOUR_MX_IP -all"')
-    print(domain_name + '.com. 1800 IN TXT "v=spf1 mx ip4:YOUR_MX_IP -all"')
-    print("Remember to replace 'YOUR_MX_IP' with the IP address of your mail server.")
-    return domain_name;
+    return domain_name
+
+def get_domain_ip():
+    while True:
+        ip = input("Enter the IP address of your domain: ")
+        if (re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
+            return ip
+
+def dns_recommendation_spf(domain_name, ip):
+    print("Add the following TXT record to your DNS:")
+    print('*.' + domain_name + '. 1800 IN TXT "v=spf1 mx ip4' + ip + ' -all"')
+    print(domain_name + '.com. 1800 IN TXT "v=spf1 mx ip4:' + ip + ' -all"')
 
 def generate_dkim_key(domain_name):
     cmd = "opendkim-genkey -b 2048 -d " + domain_name + "-s " + domain_name + ".dkim"
@@ -148,32 +166,70 @@ SigningTable refile:/etc/opendkim/signtable
 OversignHeaders From""")
 
 def configure_signtable(domain_name):
-    with open("/etc/opendkim/signtable", 'w') as file:
+    with open("/etc/opendkim/signtable", 'w+') as file:
         file.write("*@" + domain_name + " " + domain_name)
 
 def configure_keytable(domain_name):
-    with open("/etc/opendkim/keytable", 'w') as file:
+    with open("/etc/opendkim/keytable", 'w+') as file:
         wrt = domain_name + " " + domain_name + ":mail:/path/to/" + domain_name + ".dkim.private"
         file.write(wrt)
 
-def configure_internalhosts(domain_name):
-    with open("/etc/internalhosts") as file:
+def configure_internalhosts(domain_name, ip):
+    with open("/etc/internalhosts", 'w+') as file:
         file.write("your." + domain_name + "\n")
         file.write(domain_name + "\n")
-        while True:
-            ip = input("Enter the IP address of your domain: ")
-            if (re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
-                file.write(ip)
-                break
+        file.write(ip)
 
 def dns_recommendation_dkim(domain_name):
-    print("Add the following to your DNS record:")
-    path = "/etc/" + domain_name + ".dkim.txt"
+    print("Add the following TXT record to your DNS:")
     dns_entry = ""
-    with open(path, 'r') as file:
+    with open("default.txt", 'r') as file:
         dns_entry = file.read()
-    print("mail._domainkey." + domain_name + " 1800 IN TXT " + dns_entry)
-        
+    dns_entry = re.findall(r"\((.*?)\)", dns_entry)
+    print("mail._domainkey." + domain_name + " 1800 IN TXT" + dns_entry)
+
+def dns_recommendation_dmarc(domain_name):
+    print("Add the following TXT record to your DNS: ")
+    print("_dmarc." + domain_name + ' 1800 IN TXT "v=DMARC1; p=reject; rua=mailto:postmaster@' +  domain_name + '"')
+    print("Replace 'postmaster' with the email address which should collect aggregate reports.")
+
+def configure_dmarc(domain_name):
+    failureAddress = input("Enter the email address responsible for failure reports (e.g. postmaster@domain.com): ")
+    with open("/etc/opendmarc.conf", 'w') as file:
+        file.write("AuthservID mail." + domain_name + "\n")
+        file.write("""\
+PidFile /var/run/opendmarc.pid
+RejectFailures true
+Syslog true
+SyslogFacility mail
+""")
+        file.write("TrustedAuthservIDs mail." + domain_name + "\n")
+        file.write("""\
+IgnoreHosts /etc/opendmarc/ignore.hosts
+UMask 002
+UserID postfix:postfix
+TemporaryDirectory /tmp
+Socket local:/var/spool/postfix/private/opendmarc
+""")
+        file.write("FailureReportsSentBy " + failureAddress + "\n")
+        file.write("FailureReportsBcc " + failureAddress + "\n")
+        file.write("""\
+FailureReports true
+AutoRestart true
+PublicSuffixList /etc/effective_tld_names.dat
+HistoryFile /var/log/opendmarc.log""")
+
+def configure_ignorehosts(ip):
+    with open("/etc/opendmarc/ignore.hosts", 'w+') as file:
+        file.write("localhost\n")
+        file.write("127.0.0.0/8\n")
+        file.write(ip + "/24")
+
+def integrate_dmarc_dkim():
+    with open("/etc/postfix/main.cf", 'a') as file:
+        file.write("""\
+smtpd_milters = unix:private/opendkim unix:private/opendmarc
+non_smtpd_milters = unix:private/opendkim unix:private/opendmarc""")
         
 
 check_OS()
@@ -196,10 +252,17 @@ if (not check_installed_package("opendmarc")):
 if (not check_installed_package("opendkim")):
     install_opendkim()
 
-domain_name = dns_recommendation_spf()
+domain_name = get_domain_name()
+ip = get_domain_ip()
+
+dns_recommendation_spf(domain_name, ip)
 generate_dkim_key(domain_name)
 configure_dkim()
 configure_signtable(domain_name)
 configure_keytable(domain_name)
-configure_internalhosts(domain_name)
+ip = configure_internalhosts(domain_name, ip)
 dns_recommendation_dkim(domain_name)
+dns_recommendation_dmarc(domain_name)
+configure_dmarc(domain_name)
+configure_ignorehosts(ip)
+integrate_dmarc_dkim()
