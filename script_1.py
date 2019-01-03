64c96352379819917ce9279d53651507abfc95ff
Wyatt <wyatt@practicalcode.net>
2019-01-02 20:05:42 -0500

    Slashed unneeded methods; Reformatted OS print statements; Fixed all directory-related bugs

--- script_1.py
+++ script_1.py
@@ -9,117 +9,32 @@ import lsb_release as lsb
 import re
 
 def check_OS():
-    print("Checking for Ubuntu 18.04")
+    print("Checking Operating System Version")
     info = lsb.get_lsb_information()
     if (info["ID"] == "Ubuntu" and info["RELEASE"] == "18.04"):
-        print("Done")
+        print("Current operating system ({0}, {1}) is compatible".format(info["ID"], info["RELEASE"]))
         return True
     else:
-        print("Ubuntu 18.04 not detected. You must be using a different operating system.")
+        print("The current operating system is not a supported configuration.")
         exit()
 
-def check_installed_package(package):
+def check_installed_package(cache, package):
     print("Checking cache for", package)
     if cache[package].is_installed:
         print(package, "installed")
         return True
     else:
         print(package, "not found")
+        cmd = "apt install " + package
+        os.system(cmd)
+        path = "/etc/" + package
+        if (not os.path.isdir(path)):
+            os.mkdir(path)
         return False
-    
-def install_postfix():
-    print("Installing postfix with 'apt'...")
-    os.system("apt update")
-    os.system("DEBIAN_PRIORITY=low apt install postfix")
-
-def configure_mailbox():
-    mailbox = "Maildir"
-    print("""\
-Default location for the mailbox directory is 'Maildir'
-Is this location acceptable? (y/n): """)
-    cont= input()
-    while True:
-        while cont.lower() not in ("y", "n"):
-            cont = input("(y/n): ")
-        if cont == 'n':
-            virtual = input("Input custom path: ")
-            final = input("Is " + mailbox + " okay? (y/n)")
-            if final == 'y':
-                break
-        else:
-            break
-
-    print("Configuring home_mailbox...")
-    cmd = "postconf -e 'home_mailbox= " + mailbox + "/'"
-    os.system(cmd)
-    return mailbox
-
-def configure_alias_maps():
-    virtual = "/etc/postfix/virtual"
-    print("""\
-Default location for virtual_alias_maps is '/etc/postfix/virtual'
-Is this location acceptable? (y/n):""")
-    cont= input()
-    while True:
-        while cont.lower() not in ("y", "n"):
-            cont = input("(y/n): ")
-        if cont == 'n':
-            virtual = input("Input custom path: ")
-            final = input("Is " + virtual + " okay? (y/n)")
-            if final == 'y':
-                break
-        else:
-            break
-    cmd = "postconf -e 'virtual_alias_maps= hash:" + virtual + "'"
-    os.system(cmd)
-    return virtual
-
-def map_mail_addresses(virtualPath):
-    input("Press enter to edit virtual maps text file.\nWhen you are finished, save and close the file.")
-    cmd = "nano " + virtualPath
-    os.system(cmd)
-    input("Press enter to apply virtual maps.")
-    os.system("postmap /etc/postfix/virtual")
-    print("Restarting postfix to ensure changes have been applied.")
-    os.system("systemctl restart postfix")
 
 def configure_firewall():
     print("Configuring firewall...")
     os.system("sudo ufw allow Postfix")
-
-def configure_mail_environment(mailbox):
-    print("Configuring MAIL environment variable...")
-    cmd="echo 'export MAIL=~/" + mailbox + "' | sudo tee -a /etc/bash.bashrc | sudo tee -a /etc/profile.d/mail.sh"
-    os.system(cmd)
-
-def install_mail_client(mailbox):
-    cont = input("Would you like to install the s-nail package as a mail client? (y/n): ")
-    while True:
-        while cont.lower() not in ("y", "n"):
-            cont = input("(y/n): ")
-        if cont == 'y':
-            os.system("apt install s-nail")
-            with open("/etc/s-nail.rc", 'a') as file:
-                file.write("set emptystart")
-                file.write("set folider=" + mailbox)
-                file.write("set record=+sent")
-            break
-        else:
-            break
-
-def install_opendmarc():
-    os.system("apt install opendmarc")
-    try:
-        os.mkdir("/etc/opendmarc")
-    except:
-        pass
-
-def install_opendkim():
-    os.system("apt install opendkim opendkim-tools")
-    try:
-        os.mkdir("/etc/opendkim")
-    except:
-        pass
 
 def get_domain_name():
     domain_name = input("Input your domain name (e.g. domain.com): ")
@@ -127,7 +42,7 @@ def get_domain_name():
 
 def get_domain_ip():
     while True:
-        ip = input("Enter the IP address of your domain: ")
+        ip = input("Enter the IP address of your mail server: ")
         if (re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)):
             return ip
 
@@ -185,8 +100,8 @@ def dns_recommendation_dkim(domain_name):
     dns_entry = ""
     with open("default.txt", 'r') as file:
         dns_entry = file.read()
-    dns_entry = re.findall(r"\((.*?)\)", dns_entry)
-    print("mail._domainkey." + domain_name + " 1800 IN TXT" + dns_entry)
+    entry = dns_entry[dns_entry.find("(")+2 : dns_entry.find(" )")]
+    print("mail._domainkey." + domain_name + " 1800 IN TXT" + entry)
 
 def dns_recommendation_dmarc(domain_name):
     print("Add the following TXT record to your DNS: ")
@@ -237,32 +152,25 @@ check_OS()
 cache = apt.Cache()
 cache.open()
 
-if (not check_installed_package("postfix")):
-    install_postfix()
-    mailbox = configure_mailbox()
-    virtualPath = configure_alias_maps()
-    map_mail_addresses(virtualPath)
-    configure_firewall()
-    configure_mail_environment(mailbox)
-    install_mail_client(mailbox)
-
-if (not check_installed_package("opendmarc")):
-    install_opendmarc()
-
-if (not check_installed_package("opendkim")):
-    install_opendkim()
-
 domain_name = get_domain_name()
 ip = get_domain_ip()
 
-dns_recommendation_spf(domain_name, ip)
-generate_dkim_key(domain_name)
-configure_dkim()
-configure_signtable(domain_name)
-configure_keytable(domain_name)
-ip = configure_internalhosts(domain_name, ip)
-dns_recommendation_dkim(domain_name)
-dns_recommendation_dmarc(domain_name)
-configure_dmarc(domain_name)
-configure_ignorehosts(ip)
+check_installed_package(cache, "postfix")
+configure_firewall()
 integrate_dmarc_dkim()
+os.system("systemctl restart postfix")
+
+if (not check_installed_package(cache, "opendkim")):
+    check_installed_package(cache, "opendkim-tools")
+    dns_recommendation_spf(domain_name, ip)
+    generate_dkim_key(domain_name)
+    configure_dkim()
+    configure_signtable(domain_name)
+    configure_keytable(domain_name)
+    configure_internalhosts(domain_name, ip)
+    dns_recommendation_dkim(domain_name)
+
+if (not check_installed_package(cache, "opendmarc")):
+    dns_recommendation_dmarc(domain_name)
+    configure_dmarc(domain_name)
+    configure_ignorehosts(ip)
